9c7d95e6b495e275c3f2615ab21feaa216071e20
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-08-30 11:33:49 +0200

    Added tests to mshr, closes #12

--- mshr/build.sh
+++ mshr/build.sh
@@ -4,6 +4,10 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
   -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH \
+  -DENABLE_TESTS=1 \
   .
 make
 make install
+
+cd $SRC_DIR
+make test
