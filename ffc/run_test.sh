ba49572835b0de60439b59200b34b6ab860df768
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-01-07 09:01:59 +0100

    WIP: Revert use of pytest in FFC testing

--- ffc/run_test.sh
+++ ffc/run_test.sh
@@ -1,7 +1,5 @@
 export CC=$PREFIX/bin/gcc
 export CXX=$PREFIX/bin/g++
 
-pip install -i https://pypi.anaconda.org/pypi/simple pytest-xdist
-
 cd $SRC_DIR/test/unit
-py.test -n ${CPU_COUNT}
+python test.py
